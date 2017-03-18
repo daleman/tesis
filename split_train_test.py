10e9091d2939289b4c2e0eec1145f3f2bf7d8e6e
Damian Aleman <damianealeman@gmail.com>
2017-03-18 20:26:28 -0300

    split_train_test, crea los csv de train y de test con las columnas: tweet id, user_id, text

--- split_train_test.py
+++ split_train_test.py
@@ -1,20 +1,21 @@
 # coding: utf-8
 from apps import *
 import pickle
+import csv
+
 for prov in argentina:
     with open('csv/train_' + prov+ '.csv', 'rb') as fi:
         df= pickle.load(fi)
-    df['text'] = df['text'].str.replace('\n', '')
-    indexed_df = df[['tweet_id','user_id','text']]
-    indexed_df.to_csv('train/train_' + prov +'.csv',encoding='utf-8', header=None, index=False)
+    indexed_df = df.loc[:,['tweet_id','user_id','text']]
+    indexed_df['text'].replace(regex=True,inplace=True,to_replace=['\r','\n'],value=r'')
+    indexed_df.to_csv('train/train_' + prov +'.csv',encoding='utf-8', header=None, index=False,delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
     num_lines = sum(1 for line in open('train/train_'+prov+'.csv'))
     print 'train',prov, df['text'].shape,num_lines
     with open('csv/test_' + prov+ '.csv', 'rb') as fi:
         df= pickle.load(fi)
-    df['text'] = df['text'].str.replace('\n', '')
-    indexed_df = df[['tweet_id','user_id','text']]
-    indexed_df = df.set_index('tweet_id')
-    indexed_df.to_csv('test/test_' + prov +'.csv',encoding='utf-8', header=None, index=False)
+    indexed_df = df.loc[:,['tweet_id','user_id','text']]
+    indexed_df['text'].replace(regex=True,inplace=True,to_replace=['\r','\n'],value=r'')
+    indexed_df.to_csv('test/test_' + prov +'.csv',encoding='utf-8', header=None, index=False,delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
     num_lines = sum(1 for line in open('test/test_'+prov+'.csv'))
     print 'test',prov, df['text'].shape,num_lines
     
