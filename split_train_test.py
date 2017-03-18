3b7a1a209b2717d4ce2c9defb1c4b34545fff7ee
Damian Aleman <damianealeman@gmail.com>
2017-03-17 22:36:28 -0300

    train y test ahora hacen listados con los textos y los ids de los usuarios y tweets

--- split_train_test.py
+++ split_train_test.py
@@ -1,16 +1,20 @@
 # coding: utf-8
 from apps import *
+import pickle
 for prov in argentina:
     with open('csv/train_' + prov+ '.csv', 'rb') as fi:
         df= pickle.load(fi)
     df['text'] = df['text'].str.replace('\n', '')
-    df['text'].to_csv('train/train_' + prov +'.txt',encoding='utf-8', header=None, index=False)
-    num_lines = sum(1 for line in open('train/train_'+prov+'.txt'))
+    indexed_df = df[['tweet_id','user_id','text']]
+    indexed_df.to_csv('train/train_' + prov +'.csv',encoding='utf-8', header=None, index=False)
+    num_lines = sum(1 for line in open('train/train_'+prov+'.csv'))
     print 'train',prov, df['text'].shape,num_lines
     with open('csv/test_' + prov+ '.csv', 'rb') as fi:
         df= pickle.load(fi)
     df['text'] = df['text'].str.replace('\n', '')
-    df['text'].to_csv('test/test_' + prov +'.txt',encoding='utf-8', header=None, index=False)
-    num_lines = sum(1 for line in open('test/test_'+prov+'.txt'))
+    indexed_df = df[['tweet_id','user_id','text']]
+    indexed_df = df.set_index('tweet_id')
+    indexed_df.to_csv('test/test_' + prov +'.csv',encoding='utf-8', header=None, index=False)
+    num_lines = sum(1 for line in open('test/test_'+prov+'.csv'))
     print 'test',prov, df['text'].shape,num_lines
     
