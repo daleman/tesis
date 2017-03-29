ceb629a4e019bdef37ea5278ce9aec5bca7f41a0
Damian Aleman <damianealeman@gmail.com>
2017-03-28 22:34:34 -0300

    Script para separar los textos, tokenizar y generar listados)

--- sctextos.sh
+++ sctextos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Vargo el entorno virtual'
+echo 'Cargo el entorno virtual'
 source venv/bin/activate
 echo 'Creo los directorios necesarios'
 mkdir csv
@@ -12,8 +12,8 @@ mkdir regiones
 cd ..
 echo 'Empiezo a separar los usuarios y creo dataframes de train y test'
 python datosUsuarios.py
-echo 'Genero los csv con los tweets por provincia de train y test'
-python split_train_test.py
+#echo 'Genero los csv con los tweets por provincia de train y test'
+#python split_train_test.py
 echo 'Tokenizo los textos y genero las listas con diferencias significativas'
 python textos.py
 echo 'Genero las listas de pares de regiones de forma ordenada según la máxima diferencia de ocurrencias'
