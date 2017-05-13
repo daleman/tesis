1ac5c4874cec5e77e08c55e653f234ab8b32f0ff
Damian Aleman <damianealeman@gmail.com>
2017-05-13 15:32:40 -0300

    cantidad de palabras distintas

--- cantidades.py
+++ cantidades.py
@@ -4,12 +4,13 @@ import pandas as pd
 import csv
 import json
 dicc = {}
+lexico = set()
 for prov in argentina:
     df = pd.read_csv('train/train_{}.csv'.format(prov),encoding='utf-8', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     cantUsuarios = len(df.user_id.unique()) 
     cantTweets = df.shape[0]
     dicc[prov] = {'cantUsuarios':cantUsuarios,'cantTweets':cantTweets}
-    lexico = set();
+    
     with open('train/train_{}_dict.json'.format(prov)) as fi:
         prov_dict = json.load(fi)
         cantPalabras = len(prov_dict.keys())
@@ -19,4 +20,4 @@ for prov in argentina:
     dicc[prov]['cantTotal'] = cantTotal
 df1 = pd.DataFrame.from_dict(dicc,orient='index')
 df1.to_csv('cantidadesDataset.csv')
-print len(lexico)
+print 'La cantidad de palabras distintas recolectadas sobre la Argentina es: ' + str(len(lexico))
