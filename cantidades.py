7186b947b272d15cb66314369633e07ed46619c8
Damian Aleman <damianealeman@gmail.com>
2017-07-19 14:26:11 -0300

    actualizo el script de cantidades para poder usarlo sobre test

--- cantidades.py
+++ cantidades.py
@@ -5,13 +5,15 @@ import csv
 import json
 dicc = {}
 lexico = set()
+
+path = 'test/test_'
 for prov in argentina:
-    df = pd.read_csv('train/train_{}.csv'.format(prov),encoding='utf-8', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    df = pd.read_csv(path +'{}.csv'.format(prov),encoding='utf-8', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     cantUsuarios = len(df.user_id.unique()) 
     cantTweets = df.shape[0]
     dicc[prov] = {'cantUsuarios':cantUsuarios,'cantTweets':cantTweets}
     
-    with open('train/train_{}_dict.json'.format(prov)) as fi:
+    with open(path + '{}_dict.json'.format(prov)) as fi:
         prov_dict = json.load(fi)
         cantPalabras = len(prov_dict.keys())
         cantTotal = sum(prov_dict.values())
@@ -19,5 +21,5 @@ for prov in argentina:
     dicc[prov]['cantPalabras'] = cantPalabras
     dicc[prov]['cantTotal'] = cantTotal
 df1 = pd.DataFrame.from_dict(dicc,orient='index')
-df1.to_csv('cantidadesDataset.csv')
+df1.to_csv('{}cantidadesDataset.csv'.format(path))
 print 'La cantidad de palabras distintas recolectadas sobre la Argentina es: ' + str(len(lexico))
