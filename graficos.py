e2bf423f582dcbb6eb86f9df7c963c2c8d3a84d3
Damian Aleman <damianealeman@gmail.com>
2017-07-22 21:16:33 -0300

    script para graficar

--- graficos.py
+++ graficos.py
@@ -17,26 +17,26 @@ def cantPalabrasPromedio(df):
     cantPalMedia = df.groupby('user_id').cantPalabras.mean()
     ax = sns.distplot(a=cantPalMedia,hist=True,kde=False,bins=100)
     ax.set_xlabel('Cantidad de palabras promedio por usuario')
-    ax.get_figure.savefig("{0}cantPalabrasPromedio.pdf".format(relativePathImages),dpi=300)
+    ax.get_figure().savefig("{0}cantPalabrasPromedio.pdf".format(relativePathImages),dpi=300)
     plt.close(ax.get_figure())
 
-def cantPalabrasPorUsuario(df):
+def cantPalabrasUsuario(df):
     ax = sns.distplot(df.groupby('user_id').cantPalabras.sum(),hist=True,kde=False)
     ax.set_xlabel('Cantidad de palabras por usuario')
-    ax.get_figure.savefig("{0}cantPalabrasUsuario.pdf".format(relativePathImages),dpi=300)
+    ax.get_figure().savefig("{0}cantPalabrasUsuario.pdf".format(relativePathImages),dpi=300)
     plt.close(ax.get_figure())
 
-def cantTweetsPorUsuario(df):
+def cantTweetsUsuario(df):
     ax = sns.distplot(arg.groupby('user_id').cantPalabras.count(),hist=True,kde=False)
     ax.set_xlabel(u'Cantidad de tweets por usuario')
-    ax.get_figure.savefig("{0}cantTweetsUsuario.pdf".format(relativePathImages),dpi=300)
+    ax.get_figure().savefig("{0}cantTweetsUsuario.pdf".format(relativePathImages),dpi=300)
     plt.close(ax.get_figure())
 
 def histTweets1(df):
     ax = df.tweet_created_at.groupby(level=[0]).get_group('lapampa').hist(bins=100,alpha=0.5,color='red',label = 'La Pampa',stacked=True)
     arg.tweet_created_at.groupby(level=[0]).get_group('buenosaires').hist(bins=100,ax=ax,color='green',alpha=0.5,label='Buenos Aires',stacked=True)
     ax.legend()
-    ax.get_figure().savefig("{0}histTweetsProvincia{1}.pdf".format(relativePathImages),dpi=300)
+    ax.get_figure().savefig("{0}histTweetsProvincia1.pdf".format(relativePathImages),dpi=300)
     plt.close(ax.get_figure())
 
 def histTweets2(df):
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     provincias = argentina.keys()
     frames = []
     for p in provincias:
-        dfT = pd.read_csv('csv/train_{0}.csv'.format(p),encoding='utf-8',delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        dfT = pd.read_csv('csv/train_{0}.csv'.format(p),usecols=['tweet_created_at','created_at','user_id','text'],encoding='utf-8',delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
         dfT.tweet_created_at = pd.to_datetime(dfT.tweet_created_at)
         dfT.created_at = pd.to_datetime(dfT.created_at)
         frames.append(dfT)
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     arg['cantPalabras'] = arg.text.apply(lambda x: len([w for w in tknzr.tokenize(x.lower()) if w.isalpha()]))
 
     cantPalabrasPromedio(arg)
-    cantPalabrasPorUsuario(arg)
+    cantPalabrasUsuario(arg)
     cantTweetsUsuario(arg)
     histTweets1(arg)
     histTweets2(arg)
