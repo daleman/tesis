60191a6eb946a2170a7f15bb692a76d5202aa457
Damian Aleman <damianealeman@gmail.com>
2017-08-02 23:31:26 -0300

    coordenadas de usuarios de conjunto de desarrollo

--- coordenadasUsuariosTrain.py
+++ coordenadasUsuariosTrain.py
@@ -12,16 +12,18 @@ coords = {}
 path = '../tweetsFinal/pares/'
 for p in argentina:
     with open('{0}{1}_tweets.json'.format(path,p)) as f:
-                    for line in f:
-                        tweet = json.loads(line)
-                        uid = tweet['user']['id']
-                        coord = tweet['coordinates']
-                        if coord:
-                            coordenadas = str(coord['coordinates'])[1:-1]
-                            if coordenadas not in coords:
-                                coords[coordenadas] = 1
-                            else:
-                                coords[coordenadas] +=1
+        df = pd.read_csv('../train/train_{0}.csv'.format(p),encoding='utf-8', delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        usuarios = (df.user_id.unique())        
+        for line in f:
+            tweet = json.loads(line)
+            uid = tweet['user']['id']
+            coord = tweet['coordinates']
+            if coord and uid in usuarios:
+                coordenadas = str(coord['coordinates'])[1:-1]
+                if coordenadas not in coords:
+                    coords[coordenadas] = 1
+                else:
+                    coords[coordenadas] +=1
 
 with open('latex/src/resultados/coords.txt','a') as f: 
     for c,w in coords.iteritems():
