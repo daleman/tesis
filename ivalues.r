9d32a9c35404c66072a0ca9057b29ec58c494048
Damian Aleman <damianealeman@gmail.com>
2017-08-09 09:50:01 -0300

    actualizo script para test hipergeométrico

--- ivalues.r
+++ ivalues.r
@@ -1,7 +1,9 @@
 
-cant <- read.csv('cantidadesDataset.csv',row.names = 1)
-dfcantPalabrasEnProvincia <- read.csv("contrastes/provincias.csv",row.names = 1)
-df <- read.csv('notebooks/ivalue_entropia_personas_palabras_resumida.csv',row.names(1))
+cant <- read.csv('cantidadesDataset_test.csv',row.names = 1,header = TRUE)
+
+dfcantPalabrasEnProvincia <- read.csv("contrastes/provincias_test.csv",row.names = 1)
+df <- read.csv('notebooks/ivalue_entropia_personas_palabras_test.csv')
+colnames(df)[1] <- 'palabra'
 cantPalabrasTotalesEnRegion <- function(provincias){
   sum(cant[provincias,]$cantTotal)
 }
@@ -11,21 +13,29 @@ cantPalabrasWEnRegion <- function(palabra,provincias){
   sum(dfcantPalabrasEnProvincia[palabra,provs])
 }
 
-
 cantPalabraWEnArgentina <- function(palabra){
   dfcantPalabrasEnProvincia[palabra,'cantPalabra']
 }
 
+palabrasEnArgentina = sum(cant$cantTotal)#192348167
 
-palabrasEnArgentina = 192348167
 pvalor <- function(palabra,totalEnRegion,wEnRegion){
-  M = palabrasEnArgentina # Cantidad de palabras en toda la Argentina 
+  M = palabrasEnArgentina   # Cantidad de palabras en toda la Argentina 
   wEnArg = cantPalabraWEnArgentina(palabra) # Cantidad de palabras w en toda la Argentina 
-  N = totalEnRegion#cantPalabrasTotalesEnRegion(region) # Cantidad de palabras en la region
-  
-  h0 = wEnRegion#cantPalabrasWEnRegion(palabra,region)
+  N = totalEnRegion    #cantPalabrasTotalesEnRegion(region) # Cantidad de palabras en la region
+  h0 = wEnRegion#cantPalabrasWEnRegion(palabradf2,region)
   resto = palabrasEnArgentina-wEnArg
-  phyper(q=h0,m = wEnArg,n=resto,k=N,lower.tail = FALSE)
+  #cat(h0,wEnArg,resto,palabrasEnArgentina-h0,N,'\n')
+  phyper(q=h0-1,m = wEnArg,n=resto,k=N,lower.tail = FALSE,log.p = TRUE)
 }
 
-apply(X = df[,c('palabra', 'cantPalabrasTotalesEnRegion','cantPalabrasWEnRegion')],MARGIN =  1, FUN = function(x) { pvalor(x[1],as.numeric(x[2]),as.numeric(x[3]))} )
+def = read.csv('definitivo.csv')
+colnames(def)[1]<- 'palabra'
+x = def[def$Palabra.Candidata ==1,'palabra']
+x = x[1:10]   # Sacar esta línea para testear sobre todas las palabras candidatas
+df2 = df[is.element(df$palabra,x),]
+df2$pvalorLog = apply(X = df2[,c('palabra', 'cantPalabrasTotalesEnRegion','cantPalabrasWEnRegion')],MARGIN =  1, FUN = function(x) { pvalor(x[1],as.numeric(x[2]),as.numeric(x[3]))} )
+df2$pvalor = exp(df2$pvalorLog)
+
+pv=df2[with(df2, order(pvalorLog)), c("palabra",'pvalor',"pvalorLog")]
+pv$BH = p.adjust(pv$pvalor,method = 'BH')
